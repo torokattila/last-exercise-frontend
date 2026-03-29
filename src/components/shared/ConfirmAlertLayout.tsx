953ee8218218d05278f39bc833041f2dd497b9b3
Török Attila <torcsiattila93@gmail.com>
2026-03-29 14:58:58 +0200

    fix: modal issues

--- src/components/shared/ConfirmAlertLayout.tsx
+++ src/components/shared/ConfirmAlertLayout.tsx
@@ -10,32 +10,28 @@ type Props = {
 const ConfirmAlertLayout = ({ children, classNames, style }: Props) => {
   return (
     <AnimatePresence>
-      <div
-        className={`fixed top-0 left-0 z-10 h-screen w-full bg-black opacity-80`}
-      />
-      <motion.div
-        initial={{
-          scale: 0,
-        }}
-        animate={{
-          scale: 1,
-        }}
-        exit={{
-          scale: 0,
-        }}
-        transition={{
-          duration: 0.4,
-          type: 'spring',
-        }}
-        className={`
-          rounded-t-1.5 fixed inset-0 top-0 left-0 right-0 z-50 
-          my-auto mx-auto h-20 rounded-2xl bg-white p-3 align-middle 
-          opacity-100 shadow-card lg:w-65 ${classNames ?? ''}
-        `}
-        style={style}
-      >
-        {children}
-      </motion.div>
+      <div className="fixed inset-0 z-10 bg-black opacity-80" />
+      <div className="fixed inset-0 z-50 flex items-center justify-center px-5">
+        <motion.div
+          initial={{
+            scale: 0,
+          }}
+          animate={{
+            scale: 1,
+          }}
+          exit={{
+            scale: 0,
+          }}
+          transition={{
+            duration: 0.4,
+            type: 'spring',
+          }}
+          className={`w-full rounded-2xl bg-white p-3 shadow-card lg:w-65 ${classNames ?? ''}`}
+          style={style}
+        >
+          {children}
+        </motion.div>
+      </div>
     </AnimatePresence>
   );
 };
--- src/hooks/useMenu.tsx
+++ src/hooks/useMenu.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { confirmAlert } from 'react-confirm-alert';
 import { useLocation, useNavigate } from 'react-router-dom';
+import ConfirmAlertLayout from '../components/shared/ConfirmAlertLayout';
 
 const useMenu = () => {
   const location = useLocation();
@@ -60,27 +61,29 @@ const useMenu = () => {
     confirmAlert({
       customUI: ({ onClose }: { onClose: () => void }) => {
         return (
-          <div className="rounded-2xl bg-white p-3 shadow-card backdrop-blur-xl lg:p-4">
-            <p className="text-md text-left font-medium text-gray-800 lg:text-lg lg:font-bold">
-              Are you sure you want to leave this page?
-            </p>
+          <ConfirmAlertLayout>
+            <>
+              <p className="text-md text-left font-medium text-gray-800 lg:text-lg lg:font-bold">
+                Are you sure you want to leave this page?
+              </p>
 
-            <div className="mt-4 flex flex-row justify-end gap-x-2">
-              <button
-                className="rounded-full bg-[#4A9ECC] px-2 py-1 uppercase text-white transition-all hover:bg-[#0e6696]"
-                onClick={() => onClose()}
-              >
-                cancel
-              </button>
+              <div className="mt-4 flex flex-row justify-end gap-x-2">
+                <button
+                  className="rounded-full bg-[#4A9ECC] px-2 py-1 uppercase text-white transition-all hover:bg-[#0e6696]"
+                  onClick={() => onClose()}
+                >
+                  cancel
+                </button>
 
-              <button
-                className="rounded-full bg-red-500 px-2 py-1 font-bold uppercase text-white transition-all hover:bg-red-700"
-                onClick={() => handleLeavePage(onClose, page)}
-              >
-                Leave
-              </button>
-            </div>
-          </div>
+                <button
+                  className="rounded-full bg-red-500 px-2 py-1 font-bold uppercase text-white transition-all hover:bg-red-700"
+                  onClick={() => handleLeavePage(onClose, page)}
+                >
+                  Leave
+                </button>
+              </div>
+            </>
+          </ConfirmAlertLayout>
         );
       },
     });
